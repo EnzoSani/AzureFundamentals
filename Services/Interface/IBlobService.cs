bae5d311fff07b1dbafafe67121f59e4bd4a9534
Enzo <enzosanabria23@gmail.com>
2025-11-21 23:03:05 -0300

    feat: add IBlobService interface

---
+++ Services/Interface/IBlobService.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureBlobProject.Services.Interface
+{
+    public interface IBlobService
+    {
+        Task<string> GetBlob(string blobName,string containerName);
+        Task<List<string>> GetAllBlobs(string containerName);
+        Task<bool> UploadBlob(string blobName, IFormFile formFile,string containerName);
+        Task<bool> DeleteBlob(string blobName,string containerName);
+    }
+}
