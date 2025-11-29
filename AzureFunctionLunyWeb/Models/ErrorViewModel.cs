6f9348f55ffbe4ca0ba8be4a5f8567a6713c8377
Enzo <enzosanabria23@gmail.com>
2025-11-28 22:16:20 -0300

    feat: Add AzureFunctionLunyWeb project and adjust repository root

---
+++ AzureFunctionLunyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureFunctionLunyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
